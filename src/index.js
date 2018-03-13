2025f787e1004d7e49b53b80e6f30d34a1558e1c
nimwei <254060001@qq.com>
2018-03-13 21:15:21 +0800

    创建一个bundle文件，使用一个配置文件

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 /**
  * Created by nimengwei on 2018/3/13.
  */
+import _ from 'lodash';
+
 function component() {
     var element = document.createElement('div');
 
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by nimengwei on 2018/3/13.
+ */
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
