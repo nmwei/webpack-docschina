144df854aff7a89e5c6f5419a705dc9bc67bb491
nimwei <254060001@qq.com>
2018-03-13 21:59:22 +0800

    加载图片、加载CSS、加载字体

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@
  * Created by nimengwei on 2018/3/13.
  */
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
     var element = document.createElement('div');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+
+    // 将图像添加到我们现有的 div。
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
