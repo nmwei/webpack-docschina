d6729bd9726e7819ef20d81afcef1734cb67ff99
nimwei <254060001@qq.com>
2018-03-13 22:02:44 +0800

    回退处理

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@
  * Created by nimengwei on 2018/3/13.
  */
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
 
 function component() {
     var element = document.createElement('div');
