8a35f88f601724cc32a2a945dd03a62cca4ae024
nimwei <254060001@qq.com>
2018-03-16 14:27:18 +0800

    clean-webpack-plugin插件自动清理用不到的打包文件

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
  */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -10,9 +11,10 @@ module.exports = {
         print: './src/print.js'
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Output Management'
-     })
+        })
    ],
     output: {
         filename: '[name].bundle.js',
