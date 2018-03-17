670ca3b89cfeaf83772c9df5bbe5f8c10279f594
nimwei <254060001@qq.com>
2018-03-17 10:36:34 +0800

    代码分离

---
+++ src/another-module.js
@@ -0,0 +1,8 @@
+/**
+ * Created by nimengwei on 2018/3/17.
+ */
+import _ from 'lodash';
+
+console.log(
+    _.join(['Another', 'module', 'loaded!'], ' ')
+);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 /**
  * Created by nimengwei on 2018/3/13.
  */
-import { cube } from './math.js';
-
-
-if (process.env.NODE_ENV !== 'production') {
-    console.log('Looks like we are in development mode!');
-}
 
 function component() {
     var element = document.createElement('pre');
     element.innerHTML = [
         'Hello webpack!',
-        '5 cubed is equal to ' + cube(5)
+        '5 cubed is equal to '
     ].join('\n\n');
 
     return element;
