ac9b84a29a842e6d932b20123359473600242dd9
nimwei <254060001@qq.com>
2018-03-17 12:35:49 +0800

    缓存

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,38 @@
  */
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        index: './src/index.js'
+        main: './src/index.js',
+        vendor: [
+            'lodash'
+        ]
     },
     devtool: 'inline-source-map',
     plugins: [
+        new CleanWebpackPlugin(['dist']),
+
         new HTMLWebpackPlugin({
-            title: 'Code Splitting'
+            title: 'Caching'
         })
     ],
     output: {
-        filename: '[name].bundle.js',
-        chunkFilename: '[name].bundle.js',
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "common",
+                    chunks: "all"
+                }
+            }
+        }
     }
 };
