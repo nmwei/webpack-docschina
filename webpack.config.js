2ac350d8ef96386695c8b1cd52b7a47aa6e5d7f7
nimwei <254060001@qq.com>
2018-03-17 11:08:48 +0800

    防止重复(prevent duplication)

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
  * Created by nimengwei on 2018/3/17.
  */
 const path = require('path');
+const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -17,5 +18,16 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "common",
+                    chunks: "all"
+                }
+            }
+        }
     }
 };
